ac82d02095a81abae296df9e64a8411b558a9c92
Vuong, Nguyen Le <V.LNguyen@aswigsolutions.com>
2021-03-12 22:10:52 +0700

    question7

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VogCodeChallenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Running TestMethod ...");
+            QuestionClass.TestMethod();
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace VogCodeChallenge.Console
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void TestMethod()
+        {
+            int index = 0;
+            while (index < NamesList.Count)
+            {
+                System.Console.WriteLine($"Item {index + 1}: {NamesList[index]}");
+                index++;
+            }
+        }
+
+    }
+}
