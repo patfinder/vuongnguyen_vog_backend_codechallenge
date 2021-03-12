ba1055efd0579d0eae4cd6a190e74f1d363be134
Vuong, Nguyen Le <V.LNguyen@aswigsolutions.com>
2021-03-12 20:30:48 +0700

    question3

---
+++ VogCodeChallenge.API/Services/DepartmentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.Services
+{
+    public class DepartmentService
+    {
+        private static List<Department> _departments = new List<Department> {
+            new Department{ Id = "RnD", Name = "RnD Department", Address = "Floor 5"},
+            new Department{ Id = "HR", Name = "HR Department", Address = "Floor 2"},
+        };
+
+        public DepartmentService()
+        {
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _departments;
+        }
+
+        public Department Find(string id)
+        {
+            return _departments.FirstOrDefault(d => d.Id == id);
+        }
+    }
+}
---
+++ VogCodeChallenge.API/Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.Services
+{
+    public class EmployeeService
+    {
+        private static List<Employee> _employees = new List<Employee> {
+            new Employee{ Id = "Emp1", FirstName = "Abigail", LastName = "Adam", DepartmentId = "RnD", JobTitle = "Developer", Address = "1104  Breezewood Court"},
+            new Employee{ Id = "Emp2", FirstName = "Alexandra", LastName = "Adrian", DepartmentId = "RnD", JobTitle = "Developer", Address = "1104  Breezewood Court"},
+            new Employee{ Id = "Emp3", FirstName = "Alison", LastName = "Alan", DepartmentId = "RnD", JobTitle = "Developer", Address = "1104  Breezewood Court"},
+            new Employee{ Id = "Emp4", FirstName = "Amanda", LastName = "Alexander", DepartmentId = "RnD", JobTitle = "Developer", Address = "1104  Breezewood Court"},
+        };
+
+        public EmployeeService()
+        {
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return _employees;
+        }
+    }
+}
